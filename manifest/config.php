2974f0e879c193973806f8272b93038b0023b552
Stephen Bau <bauhouse@gmail.com>
2011-07-02 06:31:05 -0700

    Modify .gitignore and add configuration file

---
+++ manifest/config.php
@@ -0,0 +1,95 @@
+<?php
+
+	$settings = array(
+
+
+		###### ADMIN ######
+		'admin' => array(
+			'max_upload_size' => '5242880',
+		),
+		########
+
+
+		###### SYMPHONY ######
+		'symphony' => array(
+			'pagination_maximum_rows' => '17',
+			'allow_page_subscription' => '1',
+			'lang' => 'en',
+			'pages_table_nest_children' => 'no',
+			'version' => '2.2.1',
+			'cookie_prefix' => 'sym-',
+		),
+		########
+
+
+		###### LOG ######
+		'log' => array(
+			'archive' => '1',
+			'maxsize' => '102400',
+		),
+		########
+
+
+		###### IMAGE ######
+		'image' => array(
+			'cache' => '1',
+			'quality' => '90',
+		),
+		########
+
+
+		###### DATABASE ######
+		'database' => array(
+			'driver' => 'mysql',
+			'character_set' => 'utf8',
+			'character_encoding' => 'utf8',
+			'runtime_character_set_alter' => '1',
+			'host' => 'localhost',
+			'port' => '3306',
+			'user' => 'root',
+			'password' => '',
+			'db' => 'sym',
+			'tbl_prefix' => 'sym_',
+		),
+		########
+
+
+		###### PUBLIC ######
+		'public' => array(
+			'display_event_xml_in_source' => 'no',
+		),
+		########
+
+
+		###### GENERAL ######
+		'general' => array(
+			'useragent' => 'Symphony/2.2.1',
+			'sitename' => 'Symphony CMS',
+		),
+		########
+
+
+		###### FILE ######
+		'file' => array(
+			'write_mode' => '0775',
+		),
+		########
+
+
+		###### DIRECTORY ######
+		'directory' => array(
+			'write_mode' => '0775',
+		),
+		########
+
+
+		###### REGION ######
+		'region' => array(
+			'time_format' => 'g:i a',
+			'date_format' => 'd F Y',
+			'datetime_separator' => ' ',
+			'timezone' => 'America/Vancouver',
+		),
+		########
+	);
+
